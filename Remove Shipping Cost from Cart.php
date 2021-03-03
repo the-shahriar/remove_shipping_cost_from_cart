751ff8b5ca83e536a7274fdcffdebe99785b9015
the-shahriar <56726899+the-shahriar@users.noreply.github.com>
2021-03-03 10:51:35 +0600

    Add files via upload

---
+++ Remove Shipping Cost from Cart.php
@@ -0,0 +1,7 @@
+function disable_shipping_calc_on_cart( $show_shipping ) {
+    if( is_cart() ) {
+        return false;
+    }
+    return $show_shipping;
+}
+add_filter( 'woocommerce_cart_ready_to_calc_shipping', 'disable_shipping_calc_on_cart', 99 );
